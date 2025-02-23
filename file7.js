28ac448d7ec485cb48d989a09c60a34d1cc42e36
Grishma2477 <kandelgrishma2477@gmail.com>
2025-02-23 12:28:29 +1100

    SLice, trim and lower-uppercase added

--- file7.js
+++ file7.js
@@ -3,8 +3,19 @@
 // toLowerCase()
 // slice
 
-let firstName = "    Grishma    ";
+let firstName = "Grishma";
 
-console.log(firstName.length);
-firstName.trim();
-console.log(firstName.length);
+// console.log(firstName.length);
+// let newString= firstName.trim(); //"Grishma"
+// console.log(newString)
+// console.log(newString.length);
+
+// firstName = firstName.toLowerCase();
+// console.log(firstName);
+
+
+// start index
+// end index
+
+let newString = firstName.slice(0,4) // gris
+console.log(newString);
