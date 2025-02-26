9e4884ecbe168e28219f66f806d87d635e272d43
Grishma2477 <kandelgrishma2477@gmail.com>
2025-02-27 09:02:59 +1100

    Booleans and Comparison Opeartor Added

---
+++ file12.js
@@ -0,0 +1,20 @@
+// booleans & comparison operator
+// booleans 
+// true, false
+
+// let num1 = "7";
+// let num2 = 7;
+
+// console.log(num1>=num2);
+
+// == vs ===
+// console.log(num1 == num2); // only look values not data type
+// console.log(num1 === num2); // looks at data type as well
+
+// != vs !==
+
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
+
+
+
