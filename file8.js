065ba2f357c5a8b4eb1dea4dc2a42e7ab48458f9
Grishma2477 <kandelgrishma2477@gmail.com>
2025-02-27 08:32:05 +1100

    String Concatenation Done

---
+++ file8.js
@@ -0,0 +1,32 @@
+// type of operator
+// data types (primitive data types)
+// string "grishma"
+// number 2,4,5,6
+// booleans
+// undefined
+// null
+// BigInt
+// Symbol
+
+
+// let age = 22;
+// let firstName = "grishma";
+
+// // console.log(typeof firstName);
+// // console.log(typeof "grishma");
+// // console.log(typeof 7);
+
+// // convert number to string.
+// // console.log(typeof(age = age + "")); // 22-- "22"
+
+// age = age + "";
+// console.log(typeof(age));
+
+// //convert string to number.
+
+// let myString = + "34";
+// console.log(typeof myString)
+
+// let age = 18;
+// age = String(age); // just write string or number
+// console.log(typeof age);
---
+++ file9.js
@@ -0,0 +1,13 @@
+// string concatenation
+
+// let string1 = "grishma";
+// let string2 = "kandel";
+
+// let fullName = string1 + " " + string2
+// console.log(fullName);
+
+// let string1 = "17";
+// let string2 = "10";
+
+// let newString = +string1 + +string2
+// console.log(newString);
