e631445f366ec46a24569edd8d80a4c8d7b7fecf
Grishma2477 <kandelgrishma2477@gmail.com>
2025-02-27 08:54:31 +1100

    Undefined, Null, BigInt Added

--- file11.js
+++ file11.js
@@ -1,5 +1,22 @@
 // undefined
 // null
 
-let firstName;
-console.log(typeof firstName);
+// let firstName;
+// console.log(typeof firstName);
+// firstName = "Grishma";
+// console.log(typeof firstName, firstName);
+
+
+// let myVariable = null;
+// console.log(myVariable);
+// myVariable = "grishma";
+// console.log(myVariable, typeof myVariable);
+// console.log(typeof null);
+// // bug, error
+
+// // Big Int
+// let myNumber = BigInt(12);
+// let sameMyNumber = 123n;
+// // console.log(myNumber); 
+// // console.log(Number.MAX_SAFE_INTEGER);
+// console.log(myNumber + sameMyNumber);
