d1fb1660e877bb0396e0230f65587b5c4d81a30f
Grishma2477 <kandelgrishma2477@gmail.com>
2025-02-27 08:44:28 +1100

    Template String Done

---
+++ file10.js
@@ -0,0 +1,10 @@
+// template string
+
+// let age = 19;
+// let firstName = "Grishma Kandel";
+
+// // "My name is Grishma Kandel and my age is 19"
+
+// // let aboutMe = "My name is " + firstName + " and my age is " + age ; 
+// let aboutMe = `My name is ${firstName} and my age is ${age} `
+// console.log(aboutMe);
---
+++ file11.js
@@ -0,0 +1,5 @@
+// undefined
+// null
+
+let firstName;
+console.log(typeof firstName);
