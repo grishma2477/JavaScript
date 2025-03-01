91f8b200368c949263f6d751f61cb840c36ef085
Grishma2477 <kandelgrishma2477@gmail.com>
2025-03-02 09:23:46 +1100

    Bugs Removed

---
+++ file20.js
@@ -0,0 +1,35 @@
+// white loop
+
+// 0 to 9
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// dry don't repeat yourself.
+
+// let i = 0;
+// while(i<=9){
+//     console.log(i);
+// i++;
+// }
+// console.log(`Current value of i is ${i}`);
+// console.log("Hello");
+
+// // while loop example
+// let num = 10;
+// // let total = 0;
+// let i = 0;
+
+// // while(i<=10){
+// //     total = total + i;
+// //     i++;
+// // }
+
+// console.log(total);
+
+// let total = (num*(num+1))/2;
+// console.log(total);
+
+
+
