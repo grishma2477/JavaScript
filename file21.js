5a84adceef756f83cae7900d37a52545c2ee0c34
Grishma2477 <kandelgrishma2477@gmail.com>
2025-03-02 09:40:01 +1100

    For Loop Added

---
+++ file21.js
@@ -0,0 +1,16 @@
+// intro to for loop
+// print 0 to 9
+
+// for(let i = 0; i<=9; i++){
+//     console.log(i);
+// } 
+
+// for loop example
+let total = 0;
+let num = 100;
+ for(let i = 1; i<=num; i++){
+    total = total + i;
+ }
+
+ console.log(total);
+
