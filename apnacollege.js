05236a9de52c85ce360ff97b36201db75cc0d5c6
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-07 20:28:40 +1000

    Practice Question 5 Done

--- apnacollege.js
+++ apnacollege.js
@@ -1,11 +1,26 @@
-let color = "red";
-// traffic Light Systems
-if (color == "red"){
-    console.log("Stop!");
+// let color = "red";
+// // traffic Light Systems
+// if (color == "red"){
+//     console.log("Stop!");
+// }
+// if (color == "yellow"){
+//     console.log("Slow Down");
+// }
+// if (color == "green"){
+//     console.log("go");
+// }
+
+// to calculate popcorn prices based on the size customer asked for
+let size = "XL";
+if (size === "XL"){
+    console.log("Price is Rs.250");
 }
-if (color == "yellow"){
-    console.log("Slow Down");
+else if (size === "L"){
+    console.log("Price is Rs.200");
 }
-if (color == "green"){
-    console.log("go");
+else if (size === "M"){
+    console.log("Price is Rs.100");
+}
+else{
+    console.log('Price is Rs.50'); 
 }
