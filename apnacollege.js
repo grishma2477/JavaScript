6a43ac5c2f73e41e26252f11c6e23f11fa9b2b81
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-07 20:34:27 +1000

    Nested if else statements Added

--- apnacollege.js
+++ apnacollege.js
@@ -1,5 +1,5 @@
 // let color = "red";
-// // traffic Light Systems
+// ? traffic Light Systems
 // if (color == "red"){
 //     console.log("Stop!");
 // }
@@ -10,17 +10,24 @@
 //     console.log("go");
 // }
 
-// to calculate popcorn prices based on the size customer asked for
-let size = "XL";
-if (size === "XL"){
-    console.log("Price is Rs.250");
-}
-else if (size === "L"){
-    console.log("Price is Rs.200");
-}
-else if (size === "M"){
-    console.log("Price is Rs.100");
-}
-else{
-    console.log('Price is Rs.50'); 
+//? to calculate popcorn prices based on the size customer asked for
+// let size = "XL";
+// if (size === "XL"){
+//     console.log("Price is Rs.250");
+// }
+// else if (size === "L"){
+//     console.log("Price is Rs.200");
+// }
+// else if (size === "M"){
+//     console.log("Price is Rs.100");
+// }
+// else{
+//     console.log('Price is Rs.50'); 
+// }
+// ? Nested if-else statements
+let marks = 45;
+if (marks >= 33){
+    console.log("Pass");
+}else{
+    console.log("Better luck next time!");
 }
