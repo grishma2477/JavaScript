fe20698a5afbb38f89e121bedbff640b0fbae343
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-13 14:01:17 +1000

    Nested loops Added

--- apnacollege.js
+++ apnacollege.js
@@ -345,10 +345,18 @@
 //     console.log(i);
 // }
 
-let n = prompt("Enter your number");
-n = parseInt(n);
-for (let i =n; i<=n*10; i = i + n){
-    console.log(i);
+// let n = prompt("Enter your number");
+// n = parseInt(n);
+// for (let i =n; i<=n*10; i = i + n){
+//     console.log(i);
+// }
+
+for (let i = 1; i<=3; i++){
+    console.log(`outer loop ${i}`);
+    
+    for (let j=1; j<=3; j++){
+        console.log(j);
+    }
 }
 
 
