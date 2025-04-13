19b27a566b827032d1198fc5b4015ff1279f08ff
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-13 14:06:39 +1000

    While Loops Added

--- apnacollege.js
+++ apnacollege.js
@@ -351,12 +351,24 @@
 //     console.log(i);
 // }
 
-for (let i = 1; i<=3; i++){
-    console.log(`outer loop ${i}`);
+// for (let i = 1; i<=3; i++){
+//     console.log(`outer loop ${i}`);
     
-    for (let j=1; j<=3; j++){
-        console.log(j);
-    }
+//     for (let j=1; j<=3; j++){
+//         console.log(j);
+//     }
+// }
+
+// let i = 1;
+// while(i <=5){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 0;
+while(i <= 20){
+    console.log(i);
+    i++;
 }
 
 
