292d64d52bb619103e6a98075357d11c6ae6789f
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-13 14:48:39 +1000

    Nested Loops with nested arrays Added

--- apnacollege.js
+++ apnacollege.js
@@ -382,11 +382,19 @@
 //     console.log("congrats!!");
 // }
 
-let i = 1;
-while(i<=5){
-    console.log(i);
-}
+// let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+// for (let i = 0; i<fruits.length; i++){
+//     console.log(i, fruits[i]);
+// }
 
+let heroes = [["ironman","spiderman","thor"], ["superman", "wonder woman", "flash"]]
+for (let i =0; i<heroes.length; i++){
+    console.log(i, heroes[i]);
+    for(let j=0; j<heroes[i].length; j++){
+        console.log(`j = ${j}, ${heroes[i][j]}`);
+        
+    }
+}
 
 
 
