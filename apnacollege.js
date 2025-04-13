44d25040ee8070f88d975209add37fd9ebc58b52
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-13 13:40:56 +1000

    Practice Questions Done

--- apnacollege.js
+++ apnacollege.js
@@ -320,9 +320,22 @@
 // console.log(game);
 // console.log(game);
 
-for(let i =1; i<=10; i++){
-    console.log(i);
-}
+// for(let i =1; i <= 10; i++){
+//     console.log(i);
+// }
+// console.log(`--------------------`);
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+// for (let i=1; i<=15; i = i+2){
+//     console.log(i);
+// }
+
+// for (let i=15; i>=1; i = i-2){
+//     console.log(i);
+// }
 
 
 
