d64688cf1534fcf01fc2f72381a3761f162afa37
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-16 12:38:28 +1000

    Practice Questions Added

--- apnacollege.js
+++ apnacollege.js
@@ -725,20 +725,32 @@
 // }
 // console.log(i);
 
-function outerFunc(){
-    let x = 5;
-    let y = 6;
-    console.log("inner");
+// function outerFunc(){
+//     let x = 5;
+//     let y = 6;
+//     console.log("inner");
     
-    function innerFunc(){
-        console.log(x);
-    }
-    console.log("skipped")
-    innerFunc();
-    console.log("executing")
-}
-outerFunc();
+//     function innerFunc(){
+//         console.log(x);
+//     }
+//     console.log("skipped")
+//     innerFunc();
+//     console.log("executing")
+// }
+// outerFunc();
 
+
+let greet = "hello"; // ? global scope
+function changeGreet(){
+    let greet = "namaste"; //? function scope
+    console.log(greet);
+    function innerGreet(){
+        console.log(greet); //? lexical scope
+    }
+    innerGreet();
+}
+console.log(greet);
+changeGreet();
 
 
 
