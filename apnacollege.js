2d19c8a269a9ad08c7bc38bd832da90e304ddf8c
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-19 16:28:48 +1000

    Reduce Method Added

--- apnacollege.js
+++ apnacollege.js
@@ -986,9 +986,20 @@
 
 // console.log(countVowel("Grishma"));
 
- let nums = [10,20,30,40];
- let ans = nums.every((el) => el%10==0);
- console.log(ans);
+//  let nums = [10,20,30,40];
+//  let ans = nums.every((el) => el%10==0);
+//  console.log(ans);
+
+ let nums = [10, 20, 30, 40, 2, 4];
+ let min = nums.reduce((min,el)=>{
+    if(min<el){
+        return min;
+    }else{
+        return el;
+    }
+ })
+ console.log(min);
+ 
  
 
 
