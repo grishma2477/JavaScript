c6126e4d8d66381907cfee875833d06504afd92e
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-19 16:55:06 +1000

    String and Object combined with help of spread

--- apnacollege.js
+++ apnacollege.js
@@ -1011,17 +1011,20 @@
 //  nums = [...odd, ...even];
 //  console.log(nums);
 
- const data = {
-email: "grishma@gmail.com",
-password: "abcd",
- }
+//  const data = {
+// email: "grishma@gmail.com",
+// password: "abcd",
+//  }
 
- const dataCopy = {...data, id: 19};
- console.log(dataCopy);
+//  const dataCopy = {...data, id: 19};
+//  console.log(dataCopy);
 
- let arr = [1,2,3,4,5];
- let obj1 = {...arr};
- console.log(obj1);
+//  let arr = [1,2,3,4,5];
+//  let obj1 = {...arr};
+//  console.log(obj1);
+ 
+ let obj2 = {..."hello"};
+ console.log(obj2);
  
 
  
