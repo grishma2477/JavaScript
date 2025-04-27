a725a134fa76b01fc7ab98d56536a6bb42b53df6
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-27 12:21:30 +1000

    New Activity Done

--- apnacollege.js
+++ apnacollege.js
@@ -1054,8 +1054,49 @@
 // let {username: user, password: pass, city="Mumbai"} = student;
 // console.log(student);
 
+// let btns = document.querySelector("button");
+// console.dir(btn);
+// // btn.onclick = function(){
+// //     console.log("button was clicked.");
+    
+// // }
+
+// for(btn of btns){
+//     btn.onclick = sayHello;
+// }
+// // console.dir(btn);
+
+
+// function sayHello(){
+//     alert("Hello!");
+// }
+
+// btn.onclick = sayHello;
 
  
+// console.dir(document);
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+    console.log("Color Updated.");
+    
+});
+
+function getRandomColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+
+    let color = `rgb(${red}, ${green}, ${blue})`
+    return color;
+
+}
 
  
 
