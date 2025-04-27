031070606522731bbfb7836af9dc8e8253a7ccd6
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-27 18:41:43 +1000

    CallBack Nesting Added

--- apnacollege.js
+++ apnacollege.js
@@ -1151,15 +1151,32 @@
 // console.log(b);
 // console.log(a+b);
 
-setTimeout(()=>{
-    console.log("Apna College");
+// setTimeout(()=>{
+//     console.log("Apna College");
     
-},2000);
+// },2000);
 
-console.log("hello...");
+// console.log("hello...");
 
+h1 = document.querySelector("h1");
 
+function changeColor(color, delay, nextColorChange){
+    setTimeout(()=>{
+        h1.style.color = color;
+       if (nextColorChange) nextColorChange();
+    }, delay);
+ 
+}
 
+changeColor("red", 1000, ()=>{
+    changeColor("orange", 1000, ()=>{
+        changeColor("green", 1000,()=>{
+            changeColor("yellow", 1000, ()=>{
+                changeColor("pink", 1000);
+            });
+        });
+    });
+});
 
 
 
