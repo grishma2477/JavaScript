a8dc81b8c89e836cf02578fdd5c70d2ef55b6380
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-27 18:28:48 +1000

    Explained JS being Single threaded

--- apnacollege.js
+++ apnacollege.js
@@ -1111,23 +1111,52 @@
 // });
 
 
-function hello(){
-    console.log("inside hello function.");
+// function hello(){
+//     console.log("inside hello function.");
     
-    console.log("hello");
+//     console.log("hello");
     
-}
+// }
 
-function demo(){
-    console.log("calling hello function.");
+// function demo(){
+//     console.log("calling hello function.");
     
-    hello();
-}
+//     hello();
+// }
 
-console.log("calling demo function.");
+// console.log("calling demo function.");
 
-demo();
-console.log("Done.Bye!.");
+// demo();
+// console.log("Done.Bye!.");
+
+
+// function one(){
+//     return 1;
+// }
+
+// function two(){
+//     return one() + one();
+// }
+
+// function three(){
+//     let ans = two() + one();
+//     console.log(ans);
+// }
+// three();
+
+
+// let a = 25;
+// console.log(a);
+// let b = 10;
+// console.log(b);
+// console.log(a+b);
+
+setTimeout(()=>{
+    console.log("Apna College");
+    
+},2000);
+
+console.log("hello...");
 
 
 
