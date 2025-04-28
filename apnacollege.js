0cc49c7fe20a1e9c4c677ecfda404036dde0a1ca
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-29 09:40:54 +1000

    Promises Added

--- apnacollege.js
+++ apnacollege.js
@@ -1158,29 +1158,52 @@
 
 // console.log("hello...");
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange){
-    setTimeout(()=>{
-        h1.style.color = color;
-       if (nextColorChange) nextColorChange();
-    }, delay);
+// function changeColor(color, delay, nextColorChange){
+//     setTimeout(()=>{
+//         h1.style.color = color;
+//        if (nextColorChange) nextColorChange();
+//     }, delay);
  
+// }
+
+// changeColor("red", 1000, ()=>{
+//     changeColor("orange", 1000, ()=>{
+//         changeColor("green", 1000,()=>{
+//             changeColor("yellow", 1000, ()=>{
+//                 changeColor("pink", 1000);
+//             });
+//         });
+//     });
+// });
+
+// function savetoDb(data){
+//     console.log("your data was saved.");
+    
+// }
+
+function saveToDb(data){
+    return new Promise((resolve, reject)=>{
+        let internetSpeed = Math.floor(Math.random()*10)+1;
+        if(internetSpeed > 4){
+            resolve("success: data was saved");
+        }else{
+            reject("failure: weak connection");
+        }
+    });
 }
 
-changeColor("red", 1000, ()=>{
-    changeColor("orange", 1000, ()=>{
-        changeColor("green", 1000,()=>{
-            changeColor("yellow", 1000, ()=>{
-                changeColor("pink", 1000);
-            });
-        });
-    });
-});
+// let request = saveToDb("apna college"); // req = promise object.
+// request.then(()=> {
+//     console.log("promise was resolved.");
+    
+// })
 
-
-
-
+// .catch(()=> {
+//     console.log("promise was rejected.");
+    
+// })
 
 
 
