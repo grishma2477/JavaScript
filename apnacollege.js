934c9562453c22ebddac008345000dcf3d0b854a
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-30 09:35:34 +1000

    Handling rejections with Await Keyword Added

--- apnacollege.js
+++ apnacollege.js
@@ -1163,6 +1163,10 @@ h1 = document.querySelector("h1");
 function changeColor(color, delay){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
+            let num = Math.floor(Math.random() * 5) + 1;
+            if (num >3 ){
+                reject("promise rejected.");
+            }
             h1.style.color = color;
             console.log(`color changed to ${color}`);
             
@@ -1172,10 +1176,24 @@ function changeColor(color, delay){
 }
 
 async function demo(){
-    await changeColor("red", 1000);
-     await changeColor("green", 1000);
-    await changeColor("orange", 1000);
-    await changeColor("blue", 1000);
+    try{
+        await changeColor("red", 1000);
+        await changeColor("green", 1000);
+       await changeColor("orange", 1000);
+       await changeColor("blue", 1000);
+    } catch(err){
+        console.log("error caught.");
+        console.log(err);
+        
+    }
+   
+
+    let a = 5;
+    console.log(a);
+    console.log("new number = ", a +3);
+    
+    
+    
 }
 
 // changeColor("red", 1000)
