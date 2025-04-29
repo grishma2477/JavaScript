6926c835cc72aa718c23ab867568939298cca8ef
Grishma2477 <kandelgrishma2477@gmail.com>
2025-04-30 09:11:41 +1000

    Aysnc Functions Added

--- apnacollege.js
+++ apnacollege.js
@@ -1158,34 +1158,34 @@
 
 // console.log("hello...");
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            h1.style.color = color;
-         resolve("color changed!");
-        }, delay);
-    })
-}
+// function changeColor(color, delay){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             h1.style.color = color;
+//          resolve("color changed!");
+//         }, delay);
+//     })
+// }
 
-changeColor("red", 1000)
-.then(()=>{
-    console.log("red color was completed");
-    return changeColor("orange", 1000);
-})
-.then(()=>{
-    console.log("orange color was completed");
-    return changeColor("green", 1000);
-})
-.then(()=>{
-    console.log("green color was completed");
-    return changeColor("blue",1000);
-})
-.then(()=>{
-    console.log("blue color was completed");
+// changeColor("red", 1000)
+// .then(()=>{
+//     console.log("red color was completed");
+//     return changeColor("orange", 1000);
+// })
+// .then(()=>{
+//     console.log("orange color was completed");
+//     return changeColor("green", 1000);
+// })
+// .then(()=>{
+//     console.log("green color was completed");
+//     return changeColor("blue",1000);
+// })
+// .then(()=>{
+//     console.log("blue color was completed");
     
-});
+// });
 
 // changeColor("red", 1000, ()=>{
 //     changeColor("orange", 1000, ()=>{
@@ -1232,7 +1232,21 @@ changeColor("red", 1000)
     
 // })
 
+async function greet(){
+    throw "404 page not found.";
+    return "hello!";
+}
 
+greet()
+.then((result)=>{
+    console.log("promise was resolved");
+    console.log("result was", result);
+    
+})
+.catch((err)=>{
+    console.log("promise was rejected with err", err);
+    
+});
 
 
 
