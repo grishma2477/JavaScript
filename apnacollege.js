bd708921ed823c9f6d16442ea41bd06e4fdde552
Grishma2477 <kandelgrishma2477@gmail.com>
2025-05-02 09:39:39 +1000

    JSON Data Added

--- apnacollege.js
+++ apnacollege.js
@@ -1158,43 +1158,43 @@
 
 // console.log("hello...");
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            let num = Math.floor(Math.random() * 5) + 1;
-            if (num >3 ){
-                reject("promise rejected.");
-            }
-            h1.style.color = color;
-            console.log(`color changed to ${color}`);
+// function changeColor(color, delay){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             let num = Math.floor(Math.random() * 5) + 1;
+//             if (num >3 ){
+//                 reject("promise rejected.");
+//             }
+//             h1.style.color = color;
+//             console.log(`color changed to ${color}`);
             
-         resolve("color changed!");
-        }, delay);
-    });
-}
+//          resolve("color changed!");
+//         }, delay);
+//     });
+// }
 
-async function demo(){
-    try{
-        await changeColor("red", 1000);
-        await changeColor("green", 1000);
-       await changeColor("orange", 1000);
-       await changeColor("blue", 1000);
-    } catch(err){
-        console.log("error caught.");
-        console.log(err);
+// async function demo(){
+//     try{
+//         await changeColor("red", 1000);
+//         await changeColor("green", 1000);
+//        await changeColor("orange", 1000);
+//        await changeColor("blue", 1000);
+//     } catch(err){
+//         console.log("error caught.");
+//         console.log(err);
         
-    }
+//     }
    
 
-    let a = 5;
-    console.log(a);
-    console.log("new number = ", a +3);
+//     let a = 5;
+//     console.log(a);
+//     console.log("new number = ", a +3);
     
     
     
-}
+// }
 
 // changeColor("red", 1000)
 // .then(()=>{
@@ -1296,10 +1296,16 @@ async function demo(){
 //     await getNum();
 // }
 
+// let obj = {
+//     a: undefined,
+// };
 
 
-
-
+let student = {
+    name: "grishma",
+    marks: 100,
+};
+console.log(JSON.stringify(student));
 
 
 
