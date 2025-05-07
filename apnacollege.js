f63fd94a177bb8ddd80bbd49ad50ac9cd885587a
Grishma2477 <kandelgrishma2477@gmail.com>
2025-05-07 20:58:43 +1000

    Axios Added

--- apnacollege.js
+++ apnacollege.js
@@ -1307,30 +1307,63 @@
 // };
 // console.log(JSON.stringify(student));
 
+// let url = "https://catfact.ninja/fact";
+
+// fetch(url)
+// .then((response)=>{
+//     console.log(response);
+//      return response.json();
+//    })
+// .then((data)=>{
+//     console.log("data1 =", data.fact); 
+//     return fetch(url);
+// })
+// .then((response)=>{
+//     return response.json();
+// })
+// .then((data2)=>{
+//     console.log("data2 =", data2.fact);
+// })
+// .catch((err)=>{
+//     console.log("Error -", err);
+    
+// });
+
+// console.log("I am happy.");
+
+// async function getFacts(){
+//    try{
+//     let res = await fetch(url);
+//     let data = await res.json();
+//     console.log(data.fact);
+//    }catch(e){
+//     console.log("error -", e);
+    
+//    }
+//     console.log("bye");
+    
+// }
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async()=>{
+    let fact = await getFacts();
+    console.log(fact);
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+});
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
-.then((response)=>{
-    console.log(response);
-     return response.json();
-   })
-.then((data)=>{
-    console.log("data1 =", data.fact); 
-    return fetch(url);
-})
-.then((response)=>{
-    return response.json();
-})
-.then((data2)=>{
-    console.log("data2 =", data2.fact);
-})
-.catch((err)=>{
-    console.log("Error -", err);
-    
-});
-
-console.log("I am happy.");
-
+async function getFacts(){
+    try{
+            let res = await axios.get(url);
+            return res.data.fact;
+           }catch(e){
+            console.log("error -", e);
+            return "No Fact Found.";
+           }
+}
 
 
 
