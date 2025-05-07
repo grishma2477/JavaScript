fe8e31011f5d597983f2752f7b3bdc7738f9dc25
Grishma2477 <kandelgrishma2477@gmail.com>
2025-05-07 21:09:19 +1000

    Dog Images API Added

--- apnacollege.js
+++ apnacollege.js
@@ -1346,22 +1346,42 @@
 
 
 let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
 
+// btn.addEventListener("click", async()=>{
+//     let fact = await getFacts();
+//     console.log(fact);
+//     let p = document.querySelector("#result");
+//     p.innerText = fact;
+// });
+// let url = "https://catfact.ninja/fact";
+
+// async function getFacts(){
+//     try{
+//             let res = await axios.get(url);
+//             return res.data.fact;
+//            }catch(e){
+//             console.log("error -", e);
+//             return "No Fact Found.";
+//            }
+// }
 btn.addEventListener("click", async()=>{
-    let fact = await getFacts();
-    console.log(fact);
-    let p = document.querySelector("#result");
-    p.innerText = fact;
+   let link = await getImage();
+//    console.log(link);
+   let img = document.querySelector("#result");
+   img.setAttribute("src", link);
+   console.log(link);
+   
 });
-let url = "https://catfact.ninja/fact";
 
-async function getFacts(){
+async function getImage(){
     try{
-            let res = await axios.get(url);
-            return res.data.fact;
+            let res = await axios.get(url2);
+            return res.data.message ;
+            
            }catch(e){
             console.log("error -", e);
-            return "No Fact Found.";
+            return "/";
            }
 }
 
