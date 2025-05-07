a46d14edc7c614b48d4c0e93d385e2460e87290d
Grishma2477 <kandelgrishma2477@gmail.com>
2025-05-07 19:30:43 +1000

    First API Request Donez

--- apnacollege.js
+++ apnacollege.js
@@ -1301,11 +1301,35 @@
 // };
 
 
-let student = {
-    name: "grishma",
-    marks: 100,
-};
-console.log(JSON.stringify(student));
+// let student = {
+//     name: "grishma",
+//     marks: 100,
+// };
+// console.log(JSON.stringify(student));
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+.then((response)=>{
+    console.log(response);
+     return response.json();
+   })
+.then((data)=>{
+    console.log("data1 =", data.fact); 
+    return fetch(url);
+})
+.then((response)=>{
+    return response.json();
+})
+.then((data2)=>{
+    console.log("data2 =", data2.fact);
+})
+.catch((err)=>{
+    console.log("Error -", err);
+    
+});
+
+console.log("I am happy.");
 
 
 
