0533e75f4ed20eae9bb2bdf31433d185cc994ad7
Grishma2477 <kandelgrishma2477@gmail.com>
2025-05-13 09:59:50 +1000

    Routing Done

--- index.js
+++ index.js
@@ -9,13 +9,33 @@ app.listen(port, ()=> {
     
 });
 
-app.use((req,res)=>{
-    // console.log(req);
-    console.log("request recieved");
-    let code = "<h1>Fruits</h1> <ul><li>apple</li><Li>orange</Li></ul>"
-    // res.send({
-    //     name: "apple",
-    //     color: "red",
-    // })
-    res.send(code);
+// app.use((req,res)=>{
+//     // console.log(req);
+//     console.log("request recieved");
+//     let code = "<h1>Fruits</h1> <ul><li>apple</li><Li>orange</Li></ul>"
+//     // res.send({
+//     //     name: "apple",
+//     //     color: "red",
+//     // })
+//     res.send(code);
+// });
+
+app.get("/", (req,res) => {
+    res.send("you contacted root path.")
+});
+
+app.get("/apple", (req,res) => {
+    res.send("you contacted apple path.")
+});
+
+app.get("/orange", (req,res) => {
+    res.send("you contacted orange path.")
+});
+
+app.get("*", (req,res) =>{
+    res.send("this path doesn't exist.")
+});
+
+app.post("/", (req,res) =>{
+    res.send("you sent a post request to root.")
 });
