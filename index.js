000181c99858ad26a3d0a9c03b83c1ef5a9b88bc
Grishma2477 <kandelgrishma2477@gmail.com>
2025-05-13 13:45:17 +1000

    Path Parameters and Quesry Strings Added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // console.dir(app);
 
-let port = 8080; // 3000
+let port = 3000; // 8080
 app.listen(port, ()=> {
     console.log(`app is listening on port ${port}.`);
     
@@ -28,14 +28,26 @@ app.get("/apple", (req,res) => {
     res.send("you contacted apple path.")
 });
 
-app.get("/orange", (req,res) => {
-    res.send("you contacted orange path.")
-});
+// app.get("/orange", (req,res) => {
+//     res.send("you contacted orange path.")
+// });
 
 // app.get("*", (req,res) =>{
 //     res.send("this path doesn't exist.")
 // });
 
-app.post("/", (req,res) =>{
-    res.send("you sent a post request to root.")
+// app.post("/", (req,res) =>{
+//     res.send("you sent a post request to root.")
+// });
+
+app.get("/:username/:id", (req,res) => {
+    const {username, id} = req.params;
+    console.log(req.params);
+    res.send("you contacted root path.")
+});
+
+
+app.get("/search",(req,res)=>{
+   let {q} = req.query;
+    res.send(`<h1>search results for query: ${q}</h1>`)
 });
